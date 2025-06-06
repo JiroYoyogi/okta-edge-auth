000ce31f977c0afbce98a73495b8b012c3fc5cb6
JiroYoyogi <jiro.yoyogi.v5@gmail.com>
2025-06-06 18:14:08 +0900

    ini: npm install

---
+++ index.mjs
@@ -0,0 +1,7 @@
+// 標準モジュール
+import crypto from "crypto";
+import cookie from "cookie";
+// 外部モジュール（npm install）
+import querystring from "querystring";
+import jwt from "jsonwebtoken";
+import jwkToPem from "jwk-to-pem";
